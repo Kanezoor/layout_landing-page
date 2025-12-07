aef049609d0777ec5914e08d2938b0b0fb95acbc
Vladyslav Kostiuk <k4nezoor@gmail.com>
2025-12-07 20:25:26 +0100

    fixed issues from code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.contact-us__form');
+
+  if (form) {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      form.reset();
+      window.alert('Thank you! Your message has been sent.');
+    });
+  }
+});
